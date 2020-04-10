871c9c7a9cbcaec4882e8405dba5d899b08610c5
Hasmuddin <mdaliansari33@gmail.com>
2020-04-10 21:06:55 +0530

    seat selection

--- script/book_seat.js
+++ script/book_seat.js
@@ -13,9 +13,10 @@ let curr_movie = all_movies.find(movie => {
     return movie.id == selected_movie
 })
 
+localStorage.setItem("curr_movie", JSON.stringify(curr_movie))
+
 console.log(curr_movie)
 
-let user_activity = {}
 
 function movie_info(movie) {
     // curr_movie is an object which property will helps to make card
@@ -34,51 +35,86 @@ function movie_info(movie) {
 
 function seat_arrange(movie) {
     let card = document.createElement("div")
-    let prices = JSON.parse(localStorage.getItem("select_movie")) || []
+    let select_movie = JSON.parse(localStorage.getItem("select_movie")) || []
     card.className = "col-12 col-md-6 col-lg-8 bg-light my-1"
     card.innerHTML = `<div class="row border p-2 justify-content-center" >
-                              ${Object.keys(movie.availablity).map(name => { return `<img onclick="select_seat()" title=${name} name=${name} id="seat" src="pictures/cinema-seat.svg">` })}</div>
-                        <div class="row bg-light border">
+                              ${Object.keys(movie.availablity).map(name => { return `<img onclick="select_seat()" title=${name} name=${name} class="${name} seat" src="pictures/cinema-seat.svg">` })}</div>
+                                <div class=" bg-light border border-dark shadow-lg my-2">
                                 <div class="p-1">
                                     <div class="">
-                                         <h4 class="mx-1">Seat Selected :  ${prices.length}</h4>
-                                        ${prices.map(p => { return `<span class="mx-2 border p-1"> ${p}</span>` })}
+                                         <h4 class="mx-1">Seat Selected :  ${select_movie.length}</h4>
+                                        ${select_movie.map(p => { return `<span class="mx-2 border px-1"> ${p.slice(0, 6)}</span>` })}
                                      </div>
-                                <div class=" bg-light text-right p-3"> 
-                                Total : ${movie.price} X ${prices.length} = ${prices.length * movie.price}
+                                <div class="row border border-dark bg-light justify-content-end p-1"> 
+                                 <h5> Total :  ${movie.price} x ${select_movie.length} = ${select_movie.length * movie.price}</h5>
                                     </div>
+                                <div class="row justify-content-between px-4 my-1">
+                                    <button onclick="cancelTicket()" class="btn btn-danger">Cancel</button>
+                                    <button onclick="payment(${select_movie.length * movie.price})" class="btn btn-success">Go to Payment Page</button>
+                                </div>
                                 </div>
                         </div>
-                        
-                        `
+                     `
     return card
+}
+
+function selected_seat_color() {
+    let set = JSON.parse(localStorage.getItem("select_movie")) || []
+    let all_seats = document.getElementsByClassName("seat")
+    let i = 0
+    if (set.length !== 0) {
+        while (i <= 11) {
+            console.log(all_seats[i].className)
+            if (set.includes(all_seats[i].className)) {
+                all_seats[i].style.backgroundColor = 'green'
+            }
+            i += 1
+        }
+    }
 }
 
 
 function select_seat() {
     let set = JSON.parse(localStorage.getItem("select_movie")) || []
-    if (set.includes(event.target.name)) {
-        let deselect_index = set.indexOf(event.target.name)
+    if (set.includes(`${event.target.name} seat`)) {
+        let deselect_index = set.indexOf(`${event.target.name} seat`)
         set.splice(deselect_index, 1)
         localStorage.setItem("select_movie", JSON.stringify(set))
         window.location.reload(true)
+        selected_seat_color()
     }
     else {
-        set.push(event.target.name)
+        set.push(`${event.target.name} seat`)
         localStorage.setItem("select_movie", JSON.stringify(set))
         window.location.reload(true)
-
+        selected_seat_color()
     }
 }
 
+function cancelTicket() {
+    window.location = "index.html"
+}
 
 
+function payment(price) {
+    if (price > 0) {
+        window.location = "Booking_confirm.html"
+        let select_seat = JSON.parse(localStorage.getItem("select_movie")) || []
+        let ticket_history = JSON.parse(localStorage.getItem("ticket_history")) || []
+        ticket_history.push({ "selected_seat": select_seat, "price": price, "movie": curr_movie })
 
+        localStorage.setItem("ticket_history", JSON.stringify(ticket_history))
+    }
+    else {
+        alert("Please select any seat")
+    }
 
-
+}
 
 curr_movie_book.appendChild(movie_info(curr_movie))
 curr_movie_book.appendChild(seat_arrange(curr_movie))
+selected_seat_color()
+
 
 
 
